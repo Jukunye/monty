42587a7cace5083696e20a144ef255613040c9df
Jukunye <jukunyes@gmail.com>
2023-08-23 10:58:59 +0300

    operations

---
+++ operation.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+ * operations - executes the appropriate operation based on the opcode
+ * @stack: pointer to the stack
+ * @line_number: the correspoinding line number
+ *
+ * Return: nothing
+ */
+void operations(stack_t **stack, unsigned int line_number)
+{
+	int i;
+	char *op = tokens[0];
+	instruction_t operation[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"mul", mul},
+		{"mod", mod},
+		{NULL, NULL}
+	};
+
+	for (i = 0; operation[i].opcode != NULL; i++)
+	{
+		if (strcmp(operation[i].opcode, op) == 0)
+		{
+			operation[i].f(stack, line_number);
+			return;
+		}
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, op);
+	free_array(tokens);
+	free_list(*stack);
+	exit(EXIT_FAILURE);
+}
