2aee75fc0d6378880ca4d5c746c4ff4fbd327996
Jukunye <jukunyes@gmail.com>
2023-08-23 22:04:50 +0300

    modulus

--- mul_n_mod.c
+++ mul_n_mod.c
@@ -44,7 +44,7 @@ void mod(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		free_list(*stack);
 		free_array(tokens);
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 	}
 
 	tos = pop_int(stack);
@@ -55,7 +55,7 @@ void mod(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: division by zero\n", line_number);
 		free_list(*stack);
 		free_array(tokens);
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 	}
 
 	tos1 %= tos;
