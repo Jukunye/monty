7fbd12d26547435ff8b3f4cc380a7b3ee9fdadb0
PyotSawe <potsaw01@gmail.com>
2023-08-24 20:33:44 +0300

    Added Stack handler

---
+++ count_stack.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+  * count_stack - Counts the number of elements in the stack
+  * @stack: The stack to count
+  *
+  * Return: Number of elements in the stack
+  */
+unsigned int count_stack(stack_t *stack)
+{
+	stack_t *current = stack;
+	unsigned int lenght = 0;
+
+	while (current != NULL)
+	{
+		++lenght;
+		current = current->next;
+	}
+
+	return (lenght);
+}
