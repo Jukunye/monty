9b70dc807e0e4a510eab5cea8c63ce9b9e6248ad
PyotSawe <potsaw01@gmail.com>
2023-08-24 20:31:38 +0300

    Added memory unit

---
+++ frees_stack.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+  * frees_stack - Releases all elements in the stack
+  *
+  * Return: Nothing
+  */
+void frees_stack(void)
+{
+	stack_t *temp = NULL;
+
+	if (head)
+	{
+		temp = head;
+
+		while (temp)
+		{
+			head = head->next;
+			free(temp);
+			temp = head;
+		}
+	}
+}
