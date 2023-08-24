b862145a8503cab278f42670b9bdbeddc4d2ee4c
PyotSawe <potsaw01@gmail.com>
2023-08-25 00:50:00 +0300

    Added rotleft unit implemetation

---
+++ op_rotl.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+  * rotl - Rotates the stack to the top
+  * @stack: The head of the stack
+  * @line_number: The line on which the error occurred
+  *
+  * Return: Nothing
+  */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *curr = *stack;
+	unsigned int temp = 0;
+	(void) line_number;
+
+	if (curr && curr->next)
+	{
+		while (curr->next != NULL)
+		{
+			temp = curr->n;
+			curr->n = curr->next->n;
+			curr->next->n = temp;
+			curr = curr->next;
+		}
+	}
+}
