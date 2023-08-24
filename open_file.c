d2139f046b6cc5a07c415efd93b142ff9336a96b
PyotSawe <potsaw01@gmail.com>
2023-08-24 20:18:58 +0300

    Added File Module

---
+++ open_file.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+  * open_file - Opens a file to interpret the commands within
+  * @filename: The file to be opened
+  *
+  * Return: The file descriptor of the opened file
+  */
+FILE *open_file(char *filename)
+{
+	FILE *fd = NULL;
+
+	check_access_rights(filename);
+
+	fd = fopen(filename, "r");
+
+	if (!fd)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	return (fd);
+}
